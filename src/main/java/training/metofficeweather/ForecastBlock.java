a6ecf31bf435fea94521298bc6bd58f95e50132a
Kirsten Land <Kirsten.Land@softwire.com>
2020-11-23 14:55:39 +0000

    add display method to forecast blocks

--- src/main/java/training/metofficeweather/ForecastBlock.java
+++ src/main/java/training/metofficeweather/ForecastBlock.java
@@ -5,10 +5,22 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ForecastBlock {
-    @JsonProperty(value="F")
-    private String f;
+    @JsonProperty(value="W")
+    private int weatherCode;
 
-    public String getF() {
-        return f;
+    @JsonProperty(value="T")
+    private int temperature;
+
+    @JsonProperty(value="F")
+    private int feelsLike;
+
+    @JsonProperty(value="Pp")
+    private int precipitationProbability;
+
+    public void display() {
+        System.out.println(Toolkit.blueBoldText(Toolkit.getWeatherCode(weatherCode)));
+        System.out.println("Temperature is " + temperature + " degrees Centigrade");
+        System.out.println("Feels like " + feelsLike + " degrees Centigrade");
+        System.out.println("Chance of rain is " + precipitationProbability + " %\n");
     }
 }
--- src/main/java/training/metofficeweather/Toolkit.java
+++ src/main/java/training/metofficeweather/Toolkit.java
@@ -1,7 +1,34 @@
 package training.metofficeweather;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Toolkit {
+    private static List<String> weatherCodes = getCodes();
+
     public static String greenText(String text) {
         return "\u001b[32m" + text + "\u001b[0m";
+    }
+
+    public static String blueBoldText(String text) {
+        return "\u001b[36m\u001b[1m" + text + "\u001b[0m\u001b[0m";
+    }
+
+    public static List<String> getCodes() {
+        try {
+            List<String> codes = Files.readAllLines(Path.of("Resources/WeatherTypeLookup.txt"));
+            return codes;
+        }
+        catch (IOException e) {
+            System.out.println("Error reading weather codes");
+            return new ArrayList();
+        }
+    }
+
+    public static String getWeatherCode(int code) {
+        return weatherCodes.get(code);
     }
 }
